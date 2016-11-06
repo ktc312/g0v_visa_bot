a4dc6b5fff81f2ee483f35db5a6a5338209cc8c7
ktc312 <ktc312@nyu.edu>
2016-11-06 12:26:01 -0500

    damnit_11

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             let text = event.message.text
             if (text === 'test') {
-                sendTextMessage(sender,answers_list.slice(1, 1).toString())
+                sendTextMessage(sender,answers_list.toString())
                 continue
             }
             sendTextMessage(sender, "哈囉，你好！我是VISA BOT<3 我可以回答你簽證相關的問題唷～ 請使用你的簽證類別當作開頭，再說「我想問......」就可以了！例如「F1, 我想問OPT如何申請？」。 如果不知道要問些什麼，可以問我「你可以做什麼？」" )
